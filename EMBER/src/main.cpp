b3d9a92845a8ed72a3733cfb2fb4eb1b6d442cce
JackOLntrn <126917540+JackOLntrn@users.noreply.github.com>
2025-11-06 16:45:10 -0800

    modified:   EMBER/src/main.cpp

--- EMBER/src/main.cpp
+++ EMBER/src/main.cpp
@@ -6,25 +6,30 @@
  */
 #include <Arduino.h>
 #include "DRV8871Driver.h"
-#include <printStream.h>
 
 
 void setup() {
   Serial.begin(115200);
-  DRV8871Driver Motor1(16,17,0,10000,8);
+  // make the motor driver instance static so it persists after setup()
+  static DRV8871Driver Motor1(16,17,0,10000,8);
   Motor1.brakeMotor();
   delay(1000);
+  Serial.println("Driving motor forward at half speed");
   Motor1.driveMotor(128, true);
   delay(1000);
+  Serial.println("Braking motor");
   Motor1.brakeMotor();
   delay(200);
-  for (uint8_t speed = 0; speed <= 255; speed += 5) {
+  Serial.println("Ramping motor speed up");
+  // use an int here to avoid 8-bit wrap-around which would make the loop infinite
+  for (int speed = 0; speed <= 255; speed += 5) {
     Motor1.driveMotor(speed, true);
     delay(100);
   }
+  Serial.println("Coasting motor");
   Motor1.coastMotor();
 }
 
 void loop() {
-  Serial << "." << endl;
+  Serial.println(".");
 }
